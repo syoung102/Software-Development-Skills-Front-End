018596801862c08a63d4b11051938b8650ddf00e
seoyoung <yoon3435@inha.edu>
2022-11-21 23:21:33 +0200

    studying

---
+++ dist/js/mainjs.js
@@ -0,0 +1,34 @@
+// <div class="menu-btn">
+const menuBtn = document.querySelector('.menu-btn');
+// <nav class="menu">
+const menu = document.querySelector('.menu');
+// <ul class="menu-nav">
+const menuNav = document.querySelector('.menu-nav');
+// <div class="menu-branding">
+const menuBranding = document.querySelector('.menu-branding');
+// <li class="nav-item">
+const navItems = document.querySelectorAll('.nav-item');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu(){
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        showMenu = true;
+
+    } else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
+}
